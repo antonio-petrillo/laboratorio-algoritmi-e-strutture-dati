1bbb9bec52c315f990dbba09ced8156f68361734
antonio-petrillo <57837038+antonio-petrillo@users.noreply.github.com>
2020-05-02 19:43:21 +0200

    Add files via upload

--- progetto1/library_manager.h
+++ progetto1/library_manager.h
@@ -4,8 +4,17 @@
 #include "rbtree_library.h"
 
 typedef struct library_manager_struct{
-    rbtree tree;
-}* library_manager;
+    rbtree library;
+    char* filename;
+}* library_manager_t;
 
+
+library_manager_t make_library_manager(char* filename);
+
+void load_libray(library_manager_t library_manager);
+void load_libray_helper(rbtree library, FILE* fp);
+void store_library(library_manager_t library_manager);
+void create_backup(library_manager_t library_manager);
+void delete_library(library_manager_t library_manager);
 
 #endif
