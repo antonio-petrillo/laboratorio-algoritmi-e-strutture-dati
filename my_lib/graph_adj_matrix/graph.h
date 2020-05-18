d87dca21f70671c1dd9e7e8acc4d974100a3bada
antonio-petrillo <57837038+antonio-petrillo@users.noreply.github.com>
2020-05-19 01:33:58 +0200

    Add files via upload

--- my_lib/graph_adj_matrix/graph.h
+++ my_lib/graph_adj_matrix/graph.h
@@ -1,17 +1,20 @@
 #ifndef GRAPH_H
 #define GRAPH_H 1
 
-typedef enum{WHITE, GREY, BLACK} graph_color_t;
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+//typedef enum{WHITE, GREY, BLACK} graph_color_t;
 typedef enum{UNREACHED, REACHED, REACHED_BUT_NOT_LEFT} reach_t;
 
 typedef struct graph_struct{
-    unsigned int num_vertices;
+    size_t num_vertices;
     int** adj_matrix;
 }* graph_t;
 
 typedef struct list_graph_node_struct{
     struct list_graph_node_struct* next;
-    unsigned int vertex;
+    size_t vertex;
 }* graph_list_node_t;
 
 typedef struct stack_graph_struct{
@@ -31,53 +34,44 @@ typedef struct path_node_struct{
 
 typedef path_node_t* path_node_ptr_t;
 
-graph_t make_empty_graph(unsigned int num_vertices);
-void add_edge(graph_t G, unsigned int src, unsigned int dest, int weight);
-void remove_edge(graph_t G, unsigned int src, unsigned int dest);
-void print_graph(graph_t G);
-int is_empty_graph(graph_t G);
-void drop_graph(graph_t G);
-graph_t transpose(graph_t G);
+graph_t load_graph_from_file(FILE* fp);
+graph_t make_empty_graph(size_t num_vertices);
+void drop_graph(graph_t g);
+void print_graph(graph_t g);
+void print_full_graph_matrix(graph_t g);
+void add_vertex(graph_t g);
+void remove_vertex(graph_t g, size_t v);
+void add_edge(graph_t g, int weight, size_t dest, size_t src);
+void remove_edge(graph_t g, size_t dest, size_t src);
+graph_t transpose(graph_t g);
 
-void remove_vertex(graph_t G, unsigned int v);//logical remove
-void add_vertex(graph_t G);
-
-graph_list_node_t make_graph_list_node(unsigned int vertex);
+graph_list_node_t make_graph_list_node(size_t vertex);
 void print_graph_list_node(graph_list_node_t n);
 
 graph_stack_t make_graph_stack();
 void drop_graph_stack(graph_stack_t s);
-void push_graph_stack(graph_stack_t s, unsigned int vertex);
-int pop_graph_stack(graph_stack_t s, unsigned int* vertex);
+void push_graph_stack(graph_stack_t s, size_t vertex);
+int pop_graph_stack(graph_stack_t s, size_t* vertex);
 void print_graph_stack(graph_stack_t s);
 int is_empty_graph_stack(graph_stack_t s);
 
 graph_queue_t make_graph_queue();
 void drop_graph_queue(graph_queue_t q);
-void enqueue_graph_queue(graph_queue_t q, unsigned int vertex);
-int dequeue_graph_queue(graph_queue_t q, unsigned int* vertex);
+void enqueue_graph_queue(graph_queue_t q, size_t vertex);
+int dequeue_graph_queue(graph_queue_t q, size_t* vertex);
 void print_graph_queue(graph_queue_t q);
 int is_empty_graph_queue(graph_queue_t q);
 
-int is_white_graph(graph_color_t color);
-int is_grey_graph(graph_color_t color);
-int is_black_graph(graph_color_t color);
-
-void bfs(graph_t G, unsigned int starting_point, unsigned int end_point);
-void print_path(path_node_ptr_t reach, unsigned int starting_point, unsigned int end_point);
-void print_path_helper(path_node_ptr_t reach, unsigned int starting_point, unsigned int end_point);
-void dijkstra(graph_t G, unsigned int starting_point, unsigned int end_point);
-
-graph_t load_graph_from_file(FILE* fp);
-
-void dfs(graph_t G);
-void dfs_helper(graph_t G, path_node_ptr_t path, unsigned int starting_point);
-
-graph_stack_t ts(graph_t G);
-int ts_dfs(graph_t G, path_node_ptr_t reach, unsigned int starting_point, graph_stack_t s);
+void bfs(graph_t g, size_t end_point, size_t starting_point);
+void dijkstra(graph_t g, size_t end_point, size_t starting_point);
+void print_path(path_node_ptr_t reach, size_t starting_point, size_t end_point);
+void print_path_helper(path_node_ptr_t reach, size_t starting_point, size_t end_point);
+void dfs(graph_t g);
+void dfs_helper(graph_t g, path_node_ptr_t path, size_t starting_point);
+graph_stack_t ts(graph_t g);
+int ts_dfs(graph_t g, path_node_ptr_t reach, size_t starting_point, graph_stack_t s);
 void set_true(int* curr, int new_candidate);
-
-void scc(graph_t G);
+void scc(graph_t g);
 
 /*
 TODO: ? min spanning tree ?
--- my_lib/graph_adj_matrix/main.c
+++ my_lib/graph_adj_matrix/main.c
@@ -3,11 +3,103 @@
 #include <assert.h>
 #include "graph.h"
 
+#define DEFAULT_SIZE 10
+
+void menu();
+int choice();
+unsigned int take_vertex(char* str);
+int take_weight();
+
 int main(int argc, char** argv){
-    FILE* fp = fopen(argv[1], "r");
-    graph_t g = load_graph_from_file(fp);
-    fclose(fp);
-    print_graph(g);
-    drop_graph(g);
+    graph_t g = NULL, gt = NULL;  
+    if(argc == 2){
+        FILE* fp = fopen(argv[1], "r");
+        g = load_graph_from_file(fp);
+        fclose(fp);
+    }
+    else{
+        g = make_empty_graph(DEFAULT_SIZE);
+    }
+    int i=1;
+    while(i){
+        menu();
+        switch (choice()){
+        case 1:
+            add_edge(g, take_weight(), take_vertex("dest"), take_vertex("src"));
+            break;
+        case 2:
+            remove_edge(g, take_vertex("dest"), take_vertex("src"));
+            break;
+        case 3:
+            print_graph(g);
+            break;
+        case 4:
+            print_full_graph_matrix(g);
+            break;
+        case 5:
+            add_vertex(g);
+            break;
+        case 6:
+            remove_vertex(g, take_vertex("vertex to remove"));
+            break;
+        case 7:
+            gt = transpose(g);
+            printf("print of transposed graph\n");
+            print_graph(gt);
+            drop_graph(gt);
+            break;
+        case 8:
+            bfs(g, take_vertex("end_point"), take_vertex("starting point"));
+            break;
+        case 9:
+            dijkstra(g, take_vertex("end_point"), take_vertex("starting point"));
+            break;
+        case 10:
+            dfs(g);
+            break;
+        case 11:
+            free(ts(g));
+            break;
+        case 12:
+            scc(g);
+            break;
+        case 13:
+            drop_graph(g);
+            i = 0;
+            break;
+        default:
+            break;
+        }
+        fflush(stdin);
+    }
     return 0;
+}
+
+void menu(){
+    printf("1) add edge\n2) remove edge\n3) print graph\n");
+    printf("4) print full adj matrix\n5) add vertex\n6) remove vertex\n");
+    printf("7) make transpose & print it\n8) bfs\n9) dijkstra\n10) dfs\n11) topological sort\n12) strongly connected components\n13) drop & end\nAny other input will be ignored\n");
+    return;
+}
+
+
+int choice(){
+    int ret;
+    printf("choice: ");
+    scanf("%d", &ret);
+    return ret;
+}
+
+unsigned int take_vertex(char* str){
+    unsigned int ret;
+    printf("insert the %s: ", str);
+    scanf("%u", &ret);
+    return ret;
+}
+
+int take_weight(){
+    int ret;
+    printf("insert the weight: ");
+    scanf("%d", &ret);
+    return ret;
 }
