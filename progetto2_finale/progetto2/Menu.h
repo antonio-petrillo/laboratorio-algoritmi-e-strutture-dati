3c7a9aedc1a32c4352c7cc42faf287ce5e88e169
antonio-petrillo <57837038+antonio-petrillo@users.noreply.github.com>
2020-06-14 10:20:37 +0200

    Add files via upload

---
+++ progetto2_finale/progetto2/Menu.h
@@ -0,0 +1,20 @@
+#ifndef MENU_H
+#define MENU_H
+
+int LoginMenu(void);
+
+int AdminMenu(void);
+
+int UserMenu(void);
+
+int UserMenu1(void);
+
+int UserMenu2(void);
+
+int UserMenuLogin(void);
+
+void clearInputBuffer();
+
+#endif /* Menu_h */
+
+
